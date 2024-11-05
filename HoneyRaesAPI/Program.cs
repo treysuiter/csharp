d5621685bdbb9ca32e72d4d2b62090bf26268cbc
Trey Suiter <trey.suiter@gmail.com>
2024-11-05 17:56:39 -0600

    Add endpoint to create ServiceTickets; validate CustomerId and return created resource

--- HoneyRaesAPI/Program.cs
+++ HoneyRaesAPI/Program.cs
@@ -161,7 +161,7 @@ app.MapGet("/customers/{id}", (int id) =>
     }
 
     List<ServiceTicket> tickets = serviceTickets.Where(st => st.CustomerId == id).ToList();
-    
+
     return Results.Ok(new CustomerDTO
     {
         Id = cust.Id,
@@ -177,6 +177,39 @@ app.MapGet("/customers/{id}", (int id) =>
             DateCompleted = t.DateCompleted
         }).ToList()
     });
+});
+
+app.MapPost("/servicetickets", (ServiceTicket serviceTicket) =>
+{
+
+    // Get the customer data to check that the customerid for the service ticket is valid
+    Customer customer = customers.FirstOrDefault(c => c.Id == serviceTicket.CustomerId);
+
+    // if the client did not provide a valid customer id, this is a bad request
+    if (customer == null)
+    {
+        return Results.BadRequest();
+    }
+
+    // creates a new id (SQL will do this for us like JSON Server did!)
+    serviceTicket.Id = serviceTickets.Max(st => st.Id) + 1;
+    serviceTickets.Add(serviceTicket);
+
+    // Created returns a 201 status code with a link in the headers to where the new resource can be accessed
+    return Results.Created($"/servicetickets/{serviceTicket.Id}", new ServiceTicketDTO
+    {
+        Id = serviceTicket.Id,
+        CustomerId = serviceTicket.CustomerId,
+        Customer = new CustomerDTO
+        {
+            Id = customer.Id,
+            Name = customer.Name,
+            Address = customer.Address
+        },
+        Description = serviceTicket.Description,
+        Emergency = serviceTicket.Emergency
+    });
+
 });
 
 
